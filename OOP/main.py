2d76748a9120be22b9d287dadd60e6bc3add52c5
Helena Magaldi <helenamagaldi@gmail.com>
2020-12-14 14:16:52 -0300

    Adding OOP folder and monstergame.py (OOP)

---
+++ OOP/main.py
@@ -0,0 +1,85 @@
+import datetime as dt
+
+
+
+class TipoMonstro():
+
+    def __init__(self, nome, tipo, ps):
+
+        self.nome = nome
+
+        self.tipo = tipo
+
+        self.ps = ps
+
+
+
+        self.evolucao = 1
+
+        self.ativo = True
+
+        self.alimentos = 0
+
+        self.data_cap = dt.datetime.today()
+
+
+
+    def __str__(self):
+
+        s = ''
+
+        s += 'Nome        :%s\n' % self.nome
+
+        s += 'Tipo        :%s\n' % self.tipo
+
+        s += 'Ponto Saude :%d\n' % self.ps
+
+        s += 'Evolucao    :%d\n' % self.evolucao
+
+        s += 'Ativo       :%s\n' % self.ativo
+
+        s += 'Alimentos   :%d\n' % self.alimentos
+
+        s += 'Data Captura:%s\n' %\
+
+            self.data_cap.strftime('%d/%m/%Y')
+
+        return s
+
+
+
+    def reviver(self):
+
+        self.ativo = True
+
+
+
+    def desmaiar(sef):
+
+        # fazer
+
+        pass
+
+
+
+    def evoluir(self):
+
+        self.evolucao += 1
+
+
+
+    def alimentar(self):
+
+        # fazer
+
+        pass
+
+
+
+class TipoFogo(TipoMonstro):
+
+    def __init__(self, nome, tipo, ps, temperatura):
+
+        # fazer
+
+        pass

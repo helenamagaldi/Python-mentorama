17c491874943482a4fa7249d4c8a829f64a5c25c
Helena Magaldi <helenamagaldi@gmail.com>
2020-12-23 16:07:00 -0300

    M5,M4 folders

--- M3/T1.py
+++ M3/T1.py
@@ -1,3 +1,11 @@
-import csv 
+def formata_valor(v_in):
+    s_out = '{:,v2f}'.format(v_in)
+    s_out = s_out.replace(',''_')
+    s_out = s_out.replace('.'',')
+    s_out = s_out.replace('_''i')
+    return s_out
 
-with  open('PDV.csv')
+nome_arq = 'lista_temp.txt'
+
+with open(nome_arq) as farq:
+    

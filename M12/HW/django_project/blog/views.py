9541f4bb6435e820541d37731c9fb4436a6c50e2
Helena Magaldi <helenamagaldi@gmail.com>
2021-08-18 08:07:42 -0300

    feat new PR template, html blog templates and views

--- M12/HW/django_project/blog/views.py
+++ M12/HW/django_project/blog/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+
+posts = [
+    {
+        'author': 'helenaMagaldi',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 21, 2021',
+    },
+    {
+        'author': 'helenaMagaldi12',
+        'title': 'Blog Post 12',
+        'content': '12st post content',
+        'date_posted': 'August 22, 2021',
+    },
+    {
+        'author': 'helenaMagaldi13',
+        'title': 'Blog Post 13',
+        'content': '13th post content',
+        'date_posted': 'August 23, 2021',
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        "posts": posts
+    }
+    # return render template instead of our http response
+    # takes request as param: object
+    # still returns an http response in the background
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
+
+#  loading the template, render it and pass it to the httpresponse: django.shortcuts module helps with render
